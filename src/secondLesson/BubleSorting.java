a0a611955bec34f2f4436eec76b842492a3347a8
Olena_Oliinyk <elenikqa@ukr.net>
2019-04-04 21:46:05 +0300

    Lesson two task 7  bubble sort algorithm- ascending/descending

--- src/secondLesson/BubleSorting.java
+++ src/secondLesson/BubleSorting.java
@@ -22,5 +22,32 @@ public class BubleSorting {
             System.out.print(c[i] + " ,");
         }
     }
+    public static void arraySortedDesc(int[] c) {
+        System.out.println("Sorted  descending massive:");
+        for (int i = 0; i < c.length; i++) {
+
+            int maxValue = c[i];
+            int maxIndex = i;
+            for (int j = i + 1; j < c.length; j++) {
+
+                if (c[j] > maxValue) {
+                    maxValue = c[j];
+                    maxIndex = j;
+                }
+            }
+            if (i != maxIndex) {
+                int tmp = c[i];
+                c[i] = c[maxIndex];
+                c[maxIndex] = tmp;
+            }
+            System.out.print(c[i] + " ,");
+        }
+    }
+    public static void main(String[] args) {
+        int[] c = {11, 30, 50,100, 61, 71, 83, 1, -5, 5};
+        massive(c);
+        System.out.println();
+        arraySortedDesc(c);
+    }
 
 }
