daeb62dadefb5efcaddf43b74c0f7d29ed5cd9a5
Olena_Oliinyk <elenikqa@ukr.net>
2019-04-12 11:51:31 +0300

    Module 3 taks 1 private variables

--- src/thirdLesson/Card.java
+++ src/thirdLesson/Card.java
@@ -1,8 +1,8 @@
 package thirdLesson;
 
 public class Card {
-    public String rank;
-    public String suit;
+    private String rank;
+    private String suit;
 
    public Card(String ranks, String suits) {
 
