7afb7cc59e513b5702ba75f73fbfbf394dd92a77
Olena_Oliinyk <elenikqa@ukr.net>
2019-04-25 15:13:26 +0300

    Module 4 taks 2 third commit

--- src/fourthLesson/bankingApp/Balance.java
+++
@@ -1,9 +0,0 @@
-package fourthLesson.bankingApp;
-
-
-//The fees should be 1% for debit account
-//1% for credit account - positive balance
-//5% in case of negative balance
-public interface Balance {
-    public void updateFee(String fee);
-}
