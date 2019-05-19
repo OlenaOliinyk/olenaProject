df4c3cf96397d2f454ab676ee64a98dcce3fa287
Olena_Oliinyk <elenikqa@ukr.net>
2019-05-19 21:20:27 +0300

    Module 4 taks 2 third commit

---
+++ src/sixLesson/hashMap/Main.java
@@ -0,0 +1,24 @@
+package sixLesson.hashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Map<Passport, Citizen> people1 = new HashMap<>();
+        people1.put(new Passport("CH444444"), new Citizen("Tic", "Tok"));
+        people1.put(new Passport("CH55555"), new Citizen("Lil", "Lol"));
+        people1.put(new Passport("CH66666"), new Citizen("Pin", "Ball"));
+        for (Map.Entry<Passport, Citizen> item1 : people1.entrySet()) {
+
+            System.out.println( item1.getValue().getName() + " " + item1.getValue().getSurname());
+          System.out.println(" his passport CH444444 - "+item1.getKey().getPass().equals("CH444444"));
+
+
+
+        }
+
+    }
+}
--- src/sixLesson/hashMap/Passport.java
+++ src/sixLesson/hashMap/Passport.java
@@ -1,21 +1,21 @@
 package sixLesson.hashMap;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+
+
+
 
 public class Passport {
-    public static void main(String[] args) {
-        
-        Map<String, Citizen> people = new HashMap<>();
-        people.put("CH 408954", new Citizen("Tom", "Dock"));
-        people.put("VR 645455", new Citizen("Bill", "Duck"));
-        people.put("FE 454056", new Citizen("Nick", "Click"));
 
-        for (Map.Entry<String, Citizen> item : people.entrySet()) {
+    private String pass;
 
-            System.out.printf("Key: %s  Value: %s \n", item.getKey(), item.getValue().getName() + " " + item.getValue().getSurname());
-        }
+
+    public Passport(String valuePass) {
+
+        pass = valuePass;
+
+    }
+
+    String getPass() {
+        return pass;
     }
 }
