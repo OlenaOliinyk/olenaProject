b74bf83f311d85e0bae42492364868f2f37f6af1
Olena_Oliinyk <elenikqa@ukr.net>
2019-05-21 18:48:53 +0300

    Module 6, task 4 Commit 5 21 2019

--- src/sixLesson/hashMap/Main.java
+++ src/sixLesson/hashMap/Main.java
@@ -10,14 +10,11 @@ public class Main {
         people1.put(new Passport("CH444444"), new Citizen("Tic", "Tok"));
         people1.put(new Passport("CH55555"), new Citizen("Lil", "Lol"));
         people1.put(new Passport("CH66666"), new Citizen("Pin", "Ball"));
-        for (Map.Entry<Passport, Citizen> item1 : people1.entrySet()) {
+      //  for (Map.Entry<Passport, Citizen> item1 : people1.entrySet()) {}
 
-            System.out.println( item1.getValue().getName() + " " + item1.getValue().getSurname());
-          System.out.println(" his passport CH444444 - "+item1.getKey().getPass().equals("CH444444"));
-
-
-
-        }
+            System.out.println("User with Passport 'CH66666' is:");
+            System.out.println(people1.get(new Passport("CH66666")).getName());
+            System.out.println(people1.get(new Passport("CH66666")).getSurname());
 
     }
 }
--- src/sixLesson/hashMap/Passport.java
+++ src/sixLesson/hashMap/Passport.java
@@ -1,8 +1,7 @@
 package sixLesson.hashMap;
 
 
-
-
+import java.util.Objects;
 
 public class Passport {
 
@@ -17,5 +16,18 @@ public class Passport {
 
     String getPass() {
         return pass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Passport passport = (Passport) o;
+        return Objects.equals(pass, passport.pass);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pass);
     }
 }
